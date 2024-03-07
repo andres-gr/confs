dfe2363fe7af2d174d8bdd0aceb65c1c54ab975d
Andres Garcia <andres.gar@gmail.com>
2024-03-07 12:25:57 -0600

    fixed workspaces sh

--- hypr/scripts/workspacessetup.sh
+++ hypr/scripts/workspacessetup.sh
@@ -4,4 +4,6 @@ sleep 1
 
 hyprctl --batch "dispatch workspace 2;dispatch workspace 3;dispatch workspace 4;dispatch workspace 5;"
 hyprctl --batch "dispatch workspace 7;dispatch workspace 8;dispatch workspace 9;dispatch workspace 10;"
-hyprctl --batch "dispatch workspace 6;dispatch workspace 1;"
+
+hyprctl dispatch workspace 6
+hyprctl dispatch workspace 1
